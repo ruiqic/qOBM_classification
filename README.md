c81f575f27a4e0497d33e366ee5adbb8f3a63e55
Rui Qi Chen <ruiqic@gatech.edu>
2023-08-15 14:21:30 -0400

    setup requirements

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Cell segmentation and classification leveraging SAM and DINOv2 for qOBM T-cell i
 
 ## Installation
 
-Clone the repository locally and install with
+The code requires `python>=3.9`. Clone the repository locally and install with
 
 ```
 git clone git@github.com:ruiqic/qOBM_classification.git
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+def get_requirements(path: str):
+    return [l.strip() for l in open(path)]
+
 setup(
     name='qOBM_classification',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=get_requirements("requirements.txt"),
 )
